2330c5ae442a646a7c4384cf9f1b468bc51c43c9
jamesbyrne89 <mail@jamestbyrne.com>
2018-03-29 18:06:07 +0100

    Add favicons

--- src/components/Colour.js
+++ src/components/Colour.js
@@ -6,11 +6,13 @@ import styled from 'styled-components';
 
 const DeleteButton = styled.button`
 position: absolute;
-top: -5px;
+top: 0;
 border-radius: 2px;
-right: -10px;
-padding: 0 0.5em;
-background: #fff;
+background: transparent;
+left: -1.5rem;
+width: 1.5rem;
+height: 1.5rem;
+padding: 0.25rem;
 border: 0;
 opacity: 0;
 font-weight: bold;
@@ -18,12 +20,16 @@ pointer-events: none;
 -webkit-transition: opacity 0.15s;
 -webkit-transition: opacity 0.15s;
 transition: opacity 0.15s;
--webkit-box-shadow: 0 2px 6px var(--backgroundColour), 0 1px 3px var(--backgroundColour);
-box-shadow: 0 2px 6px var(--backgroundColour), 0 1px 3px var(--backgroundColour);
 cursor: pointer;
 `
 
-const ColourStyles = styled.div `
+const DeleteIcon = styled.svg`
+width: 100%;
+height: 100%;
+fill: #fff;
+`
+
+const ColourStyles = styled.div`
 position: relative;
 text-align: left;
 background: var(--contentBackgroundColour);
@@ -31,17 +37,18 @@ background: var(--contentBackgroundColour);
     opacity: 1;
     pointer-events: auto;
 }
-` 
-
-const ColourSwatch = styled.div `
-height: 140px;
-background: ${props => {
-    console.log(props)
-    return props.hex}
-}
 `
 
-const ColourDetails = styled.div `
+const ColourSwatch = styled.div`
+height: 140px;
+background: ${props => {
+        console.log(props)
+        return props.hex
+    }
+    }
+`
+
+const ColourDetails = styled.div`
 padding: 0.25em 0;
 text-transform: uppercase;
 position: relative;
@@ -62,7 +69,7 @@ const ColourDetailsHoverOverlay = styled.div`
     font-weight: 700;
 `
 
-const ColourHex = styled.div `
+const ColourHex = styled.div`
 padding: 0.55em 0.5em 0.45em;
 position: relative;
 background: var(--contentBackgroundColour);
@@ -72,7 +79,7 @@ overflow: hidden;
     opacity: 1;
 }
 `
-const ColourRgb = styled.div `
+const ColourRgb = styled.div`
 padding: 0.55em 0.5em 0.45em;
 position: relative;
 background: var(--contentBackgroundColour);
@@ -97,19 +104,21 @@ const Colour = (props) => {
 
     return (
         <ColourStyles>
-            <DeleteButton onClick={() => handleRemoveColour(index)} >X</DeleteButton>
-            <ColourSwatch style={{background: colour.hex}}></ColourSwatch>
+            <DeleteButton onClick={() => handleRemoveColour(index)}>
+                <DeleteIcon viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M704 1376v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm256 0v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm256 0v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm-544-992h448l-48-117q-7-9-17-11h-317q-10 2-17 11zm928 32v64q0 14-9 23t-23 9h-96v948q0 83-47 143.5t-113 60.5h-832q-66 0-113-58.5t-47-141.5v-952h-96q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h309l70-167q15-37 54-63t79-26h320q40 0 79 26t54 63l70 167h309q14 0 23 9t9 23z" /></DeleteIcon>
+            </DeleteButton>
+            <ColourSwatch style={{ background: colour.hex }}></ColourSwatch>
             <ColourDetails>
                 <CopyToClipboard text={colour.hex}
                     onCopy={() => console.log('copied')}>
                     <ColourHex>{colour.hex}
-                    <ColourDetailsHoverOverlay><span>Copy Hex</span></ColourDetailsHoverOverlay>
+                        <ColourDetailsHoverOverlay><span>Copy Hex</span></ColourDetailsHoverOverlay>
                     </ColourHex>
                 </CopyToClipboard>
                 <CopyToClipboard text={colour.hex}
                     onCopy={() => console.log('copied')}>
                     <ColourRgb>{colour.rgb}
-                    <ColourDetailsHoverOverlay><span>Copy RGB</span></ColourDetailsHoverOverlay>
+                        <ColourDetailsHoverOverlay><span>Copy RGB</span></ColourDetailsHoverOverlay>
                     </ColourRgb>
                 </CopyToClipboard>
             </ColourDetails>
